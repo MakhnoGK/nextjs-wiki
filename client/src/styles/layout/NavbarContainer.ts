fe1dcf44ca5637d50a352833a491d69372846100
Hryhorii Khyzhniakov <soults15@gmail.com>
2021-04-18 00:50:59 +0300

    Added search bar animation

--- client/src/styles/layout/NavbarContainer.ts
+++ client/src/styles/layout/NavbarContainer.ts
@@ -20,16 +20,25 @@ const NavbarContainer = styled.div`
     }
 
     .navbarSearch {
-        width: 375px;
+        width: 45vw;
+        margin: 0 0 0 -4vw;
 
         input {
+            display: block;
             font-family: inherit;
             color: ${colors.textGrayMedium};
             padding: 7px 17px;
-            width: 100%;
+            margin: 0 auto;
+            width: 50%;
             border: 1px solid ${colors.grayMedium};
             border-radius: 16px;
             outline: none;
+            transition: 0.2s ease-out;
+            transition-property: width;
+
+            &:focus {
+                width: 100%;
+            }
         }
     }
 
